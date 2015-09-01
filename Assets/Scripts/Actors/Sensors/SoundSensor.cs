2b90a52e0b83e0e7af970d94693a3f2f9ef90402
Steven Alford <steven.alford@collinsonlatitude.com>
2015-09-01 15:01:58 +0800

    Adding sensors.

---
+++ Assets/Scripts/Actors/Sensors/SoundSensor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class SoundSensor : MonoBehaviour
+{
+    private void Awake()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+    }
+}
