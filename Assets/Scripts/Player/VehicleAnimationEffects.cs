f453f4ad5aa5bc855b051e85af1b057da3329f9a
Steven Alford <steven.alford@collinsonlatitude.com>
2015-09-01 11:38:34 +0800

    Capture animation events.

---
+++ Assets/Scripts/Player/VehicleAnimationEffects.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class VehicleAnimationEffects : MonoBehaviour
+{
+    private void Awake()
+    {
+    }
+
+    private void Update()
+    {
+    }
+
+    private void LeftFootStomp()
+    {
+        Debug.Log("LEFT");
+    }
+
+    private void RightFootStomp()
+    {
+        Debug.Log("RIGHT");
+    }
+}
