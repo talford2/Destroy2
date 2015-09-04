13c2c9112d29f2eb2ea5138c30e34c529908dda8
Steven Alford <steven.alford@collinsonlatitude.com>
2015-09-04 23:52:44 +0800

    Some good fellas.

--- Assets/Scripts/Actors/Sensors/SoundSensor.cs
+++ Assets/Scripts/Actors/Sensors/SoundSensor.cs
@@ -3,21 +3,35 @@
 public class SoundSensor : MonoBehaviour
 {
     private AutonomousAgent owner;
+    private Team opposingTeam;
 
     private void Awake()
     {
         owner = GetComponentInParent<AutonomousAgent>();
+        opposingTeam = Targeting.GetOpposingTeam(owner.Team);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        var autoAgent = other.GetComponentInParent<PlayerController>();
+        var autoAgent = other.GetComponentInParent<AutonomousAgent>();
         if (autoAgent != null)
         {
-            if (autoAgent.Team == Targeting.GetOpposingTeam(owner.Team))
+            if (autoAgent.Team == opposingTeam)
             {
                 var npc = owner.GetComponent<NpcSoldierController>();
-                npc.SetTarget(autoAgent.GetVehicle());
+                var target = Targeting.FindNearest(opposingTeam, owner.GetVehicle().transform.position, 50f);
+                npc.SetTarget(target);
+                Debug.Log("SET TARGET!");
+            }
+        }
+        else
+        {
+            var player = other.GetComponentInParent<PlayerController>();
+            if (player.Team == opposingTeam)
+            {
+                var npc = owner.GetComponent<NpcSoldierController>();
+                var target = Targeting.FindNearest(opposingTeam, owner.GetVehicle().transform.position, 50f);
+                npc.SetTarget(target);
                 Debug.Log("SET TARGET!");
             }
         }
