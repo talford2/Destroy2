c4292db5fcb26ca047a6dcf1d551322825981770
Steven Alford <steven.alford@collinsonlatitude.com>
2015-09-04 12:26:20 +0800

    Create NeighbourSensor.

--- Assets/Scripts/Actors/NPCs/AutonomousAgent.cs
+++ Assets/Scripts/Actors/NPCs/AutonomousAgent.cs
@@ -1,8 +1,30 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public abstract class AutonomousAgent : MonoBehaviour
 {
     public Vector3 Heading;
+    private List<AutonomousAgent> neighbours;
 
     public abstract Vehicle GetVehicle();
+
+    public virtual void AddNeighbour(AutonomousAgent neighbour)
+    {
+        if (neighbours==null)
+            neighbours = new List<AutonomousAgent>();
+        if (!neighbours.Contains(neighbour))
+        neighbours.Add(neighbour);
+    }
+
+    public virtual void RemoveNeighbour(AutonomousAgent neighbour)
+    {
+        if (neighbours == null)
+            neighbours = new List<AutonomousAgent>();
+        neighbours.Remove(neighbour);
+    }
+
+    public virtual List<AutonomousAgent> GetNeighbours()
+    {
+        return neighbours;
+    }
 }
---
+++ Assets/Scripts/Actors/Sensors/NeighbourSensor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof (SphereCollider))]
+public class NeighbourSensor : MonoBehaviour
+{
+    private AutonomousAgent owner;
+
+    private void Awake()
+    {
+        owner = GetComponentInParent<AutonomousAgent>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var autoAgent = other.GetComponentInParent<AutonomousAgent>();
+        if (autoAgent != null)
+            owner.AddNeighbour(autoAgent);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var autoAgent = other.GetComponentInParent<AutonomousAgent>();
+        if (autoAgent != null)
+            owner.RemoveNeighbour(autoAgent);
+    }
+}
