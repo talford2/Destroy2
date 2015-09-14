d5b7a70fc93739369242c61e9f92872ef37922f2
Steven Alford <steven.alford@collinsonlatitude.com>
2015-09-14 23:14:14 +0800

    Create Spawner prefab.

---
+++ Assets/Scripts/Actors/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject SpawnPrefab;
+    public float Frequency;
+
+
+    private void Awake()
+    {
+        StartCoroutine(Spawn(Frequency));
+    }
+
+    private IEnumerator Spawn(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Instantiate(SpawnPrefab, transform.position, transform.rotation);
+        Debug.Log("SPAWN!!!");
+        StartCoroutine(Spawn(Frequency));
+    }
+}
