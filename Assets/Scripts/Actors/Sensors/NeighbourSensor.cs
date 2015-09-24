0ec5a71e9bac104e847ab036444f177950d58c1f
Steven Alford <steven.alford@collinsonlatitude.com>
2015-09-24 22:10:54 +0800

    Avoid some GetComponentInParent calls.

--- Assets/Scripts/Actors/Sensors/NeighbourSensor.cs
+++ Assets/Scripts/Actors/Sensors/NeighbourSensor.cs
@@ -1,25 +1,35 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 [RequireComponent(typeof (SphereCollider))]
 public class NeighbourSensor : MonoBehaviour
 {
     private AutonomousAgent owner;
 
+    private List<Collider> inColliders;
+    //private List<Collider> outColliders;
+
     private void Awake()
     {
         owner = GetComponentInParent<AutonomousAgent>();
+        inColliders = new List<Collider>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        var autoAgent = other.GetComponentInParent<ActorAgent>();
-        if (autoAgent != null)
-            owner.AddNeighbour(autoAgent);
-        var cover = other.GetComponentInParent<Cover>();
-        if (cover != null)
+        if (!inColliders.Contains(other))
         {
-            Debug.Log("FOUND COVER!");
-            owner.AddDetectedCover(cover);
+            inColliders.Add(other);
+            var autoAgent = other.GetComponentInParent<ActorAgent>();
+            if (autoAgent != null)
+                owner.AddNeighbour(autoAgent);
+            var cover = other.GetComponentInParent<Cover>();
+            if (cover != null)
+            {
+                Debug.Log("FOUND COVER!");
+                owner.AddDetectedCover(cover);
+            }
         }
     }
 
