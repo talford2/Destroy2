a3889658b367b81e7d523720c7b03418fca9a1f0
talford2 <talford2@hotmail.com>
2015-09-26 21:12:51 +1000

    Hoodini corpses

--- Assets/Scripts/Actors/Corpses/Corpse.cs
+++ Assets/Scripts/Actors/Corpses/Corpse.cs
@@ -1,5 +1,23 @@
 ﻿using UnityEngine;
 
-public abstract class Corpse : MonoBehaviour {
-    public Transform FocalPoint;
+public abstract class Corpse : MonoBehaviour
+{
+	public float MinLifeTime = 10f;
+	private float lifeCooldown = 0;
+
+	public Transform FocalPoint;
+
+	private void Awake()
+	{
+		lifeCooldown = MinLifeTime;
+	}
+
+	public void Update()
+	{
+		lifeCooldown -= Time.deltaTime;
+		if (lifeCooldown < 0 && !gameObject.GetComponent<Renderer>().isVisible)
+		{
+			Destroy(gameObject);
+		}
+	}
 }
