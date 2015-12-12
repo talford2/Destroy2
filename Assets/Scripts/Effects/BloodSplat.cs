1416c116f6bfc2fc29378aec856f8055ce1b302d
Steven Alford <steven.alford@collinsonlatitude.com>
2015-12-12 12:46:20 +0800

    Add blood projector effect.

---
+++ Assets/Scripts/Effects/BloodSplat.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class BloodSplat : MonoBehaviour
+{
+    public GameObject Prefab;
+
+	void Start ()
+	{
+	    var floorRay = new Ray(transform.position, Vector3.down);
+	    RaycastHit floorHit;
+	    if (Physics.Raycast(floorRay, out floorHit, 10f))
+	    {
+            var instance = Instantiate(Prefab);
+            instance.transform.position = floorHit.point + Vector3.up * 0.5f;
+	    }
+	}
+}
