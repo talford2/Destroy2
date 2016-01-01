3748a1e4f9ba28de0446c510a86cb2efd90fbee1
Steven Alford <steven.alford@collinsonlatitude.com>
2016-01-01 13:50:38 +0800

    SoldierSounds component created.

---
+++ Assets/Scripts/Vehicles/SoldierSounds.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldierSounds : MonoBehaviour
+{
+    public List<AudioClip> LeftSteps;
+    public List<AudioClip> RightSteps;
+
+    public List<AudioClip> HurtCries;
+
+    private void Awake()
+    {
+        var killable = GetComponent<Killable>();
+        killable.OnDamage += HurtCry;
+    }
+
+    private void RunLeftStep()
+    {
+        AudioSource.PlayClipAtPoint(LeftSteps[Random.Range(0, LeftSteps.Count)], transform.position);
+    }
+
+    private void RunRightStep()
+    {
+        AudioSource.PlayClipAtPoint(RightSteps[Random.Range(0, RightSteps.Count)], transform.position);
+    }
+
+    private void HurtCry(Collider hitcollider, Vector3 position, Vector3 direction, float power, float damage, GameObject attacker)
+    {
+        AudioSource.PlayClipAtPoint(HurtCries[Random.Range(0, HurtCries.Count)], transform.position);
+    }
+}
